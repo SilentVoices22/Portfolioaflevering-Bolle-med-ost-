c75988bed7f689d49e4b7457904af1e565339823
SilentVoices22 <pinyuanlu@gmail.com>
2025-11-10 10:09:25 +0100

    Update index.html and style.css; add main.js for chart functionality

---
+++ main.js
@@ -0,0 +1,11 @@
+const ctx = document.querySelector('#chart').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Amager', 'Charlottelund', 'Christianshavn', 'Esbjerg', 'Frederiksbjerg', 'Faaborg', 'Indre by', 'Kastrup','Lyngby','Nordvest','Nørrebro','Odense','Reffen','Silkeborg','Sjøllands Odde', 'Slagelse','Sydhavn','Valby','Vesterbro','Viborg','Østerbro','Århus','Aarhus'],
+        datasets: [{
+            label:  'Bmo Score',
+            data: [4.7, 4.1, 5.1, 4.5, 4.1, 2.5,4.4,1.9,3.8,5,4.5,3.9,4.1,5.6,5,2,4.8,4.1,4.6,4.2,4.4,4.4,5.2],
+        }]
+    }
+});
