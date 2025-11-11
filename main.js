b3ed596cf06e10d222db98e5d0987d2afebff9fd
Kai <kaibomholtz@gmail.com>
2025-11-11 12:32:43 +0100

    basic layout

--- main.js
+++ main.js
@@ -88,6 +88,18 @@ const chart = new Chart(ctx, {
     }
 });
 
+const ctx2 = document.querySelector('#chart-2').getContext('2d');
+const chart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['Amager', 'Charlottelund', 'Christianshavn', 'Esbjerg', 'Frederiksbjerg', 'Faaborg', 'Indre by', 'Kastrup','Lyngby','Nordvest','Nørrebro','Odense','Reffen','Silkeborg','Sjøllands Odde', 'Slagelse','Sydhavn','Valby','Vesterbro','Viborg','Østerbro','Århus','Aarhus'],
+        datasets: [{
+            label:  'Bmo Score',
+            data: [4.7, 4.1, 5.1, 4.5, 4.1, 2.5,4.4,1.9,3.8,5,4.5,3.9,4.1,5.6,5,2,4.8,4.1,4.6,4.2,4.4,4.4,5.2],
+        }]
+    }
+});
+
 // BMO script
 const BMO = document.querySelector('#billed')
 window.addEventListener("scroll", () => {
